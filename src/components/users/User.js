0de7b8e6eeb9bf6164256879acd62c2ac6ae173c
James F. Thomas <james.fthomas@yahoo.com>
2020-12-06 09:38:50 -0600

    (add) Repos && RepoItem componenets impplemeneted and displaying a users public repos

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,5 +1,6 @@
 import React, { Fragment, Component } from 'react';
 import Spinner from '../layout/Spinner';
+import Repos from '../repos/Repos'
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
@@ -16,6 +17,7 @@ export class User extends Component {
   static propTypes = {
     loading: PropTypes.bool.isRequired,
     user: PropTypes.object.isRequired,
+    repos: PropTypes.array.isRequired,
     getUser: PropTypes.func.isRequired,
     getUserRepos: PropTypes.func.isRequired,
   }
@@ -40,7 +42,7 @@ export class User extends Component {
       hireable
     } = this.props.user;
 
-    const { loading } = this.props;
+    const { loading, repos } = this.props;
 
     if(loading ){ return <Spinner/> }
 
@@ -110,8 +112,10 @@ export class User extends Component {
                 <div className='badge badge-light'> Public Repos: {public_repos}</div>
                 <div className='badge badge-dark'> Public Gists: {public_gists}</div>
         </div>
-      </Fragment>
 
+        <Repos repos={repos}/>
+
+      </Fragment>
     }
   }
 }

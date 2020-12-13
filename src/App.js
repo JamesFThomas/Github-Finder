5be947166c12307d1dc1b91650ca5573c3fc6230
James F. Thomas <james.fthomas@yahoo.com>
2020-12-12 19:16:17 -0600

    (add) NotFound componenet created to show message when wrong endpoint visited

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import User from './components/users/User';
 import Alert from './components/layout/Alert';
-import Search from './components/users/Search';
+// import Search from './components/users/Search';
 import Home from './components/Pages/Home';
 import About from './components/Pages/About';
+import NotFound from './components/Pages/NotFound';
+
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
 
@@ -29,6 +31,7 @@ const App = () => {
                   <Route exact path='/' component={Home}/>
                   <Route exact path='/about' component={About}/>
                   <Route exact path='/user/:login' component={User} />
+                  <Route component={NotFound} />
                 </Switch>
 
               </div>
